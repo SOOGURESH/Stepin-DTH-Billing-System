87edeaf83e9b8521f1c16a2400004d3a4865530d
SOOGURESH <soogureshkajagar@gmail.com>
2021-09-06 12:42:49 +0530

    implementation

---
+++ 3_Implementation/fun.h
@@ -0,0 +1,9 @@
+#ifndef _FUN_H
+#define _FUN_H
+void addrecords();
+void deleterecords();
+void searchrecords();
+void listrecords();
+void payment();
+void modifyrecords();
+#endif
---
+++ 3_Implementation/struct.h
@@ -0,0 +1,7 @@
+#include "fun.h"
+struct subscriber
+{
+char smartcardnumber[20];
+char name[50];
+float amount;
+}s;
