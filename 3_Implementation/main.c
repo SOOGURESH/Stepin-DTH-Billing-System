204f0921d666d3a18f6b50ce8454b548f01e69ff
SOOGURESH <soogureshkajagar@gmail.com>
2021-09-08 13:38:40 +0530

    changes

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
-#include<conio.h>
+//#include<conio.h>
 #include<ctype.h>
-#include<windows.h>
+//#include<windows.h>
 #include<stdlib.h>
 #include"struct.h"
 #include"fun.h"
